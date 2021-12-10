7e08d026eb7e3b21c2474616024f828f007054e9
Ghiftee <gigibetine@gmail.com>
2021-12-11 00:38:10 +0100

    Style details page

--- src/components/HomePage/Details.jsx
+++ src/components/HomePage/Details.jsx
@@ -4,6 +4,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { Table, Button } from 'react-bootstrap';
 import { logoutUser } from '../../services/authService';
 import Nav from '../Nav';
+import './Details.css';
 import { carsSelector } from '../../redux/slices/cars';
 
 function Details() {
@@ -17,13 +18,13 @@ function Details() {
     history('/login');
   };
   return (
-    <div>
+    <div className="contents p-0">
       <Nav handleLogOut={handleLogOut} />
-      <div>
-        <div>
-          <img alt="" src={currentCar.image} />
+      <div className="car-details d-flex align-items-center justify-content-between w-100 p-4">
+        <div className="image-holder">
+          <img alt="" src={currentCar.image} className="car-image" />
           <div
-            id="previousButtonContainer"
+            id="backButtonContainer"
           >
             <div
               onClick={() => {
@@ -32,37 +33,37 @@ function Details() {
               onKeyPress={() => {}}
               role="button"
               tabIndex="0"
-              id="previousButton"
+              id="backButton"
             >
               <i className="fa fa-caret-left fsize-25" />
             </div>
           </div>
         </div>
-        <div>
-          <h5>{currentCar.name.toUpperCase()}</h5>
-          <p>- $50 deposit on any purchase</p>
+        <div className="text-details">
+          <h5 className="fw-bold text-end fsize-25">{currentCar.name.toUpperCase()}</h5>
+          <p className="text-end fsize-15">- $50 deposit on any purchase</p>
           <Table striped>
             <tbody>
               <tr>
-                <td>PRICE</td>
-                <td>{`${currentCar.price}`}</td>
+                <td className="fw-bold">PRICE</td>
+                <td className="text-end">{`$ ${currentCar.price}`}</td>
               </tr>
               <tr>
-                <td>NUMBER OF SEATS</td>
-                <td>{`${currentCar.number_of_seats}`}</td>
+                <td className="fw-bold">NUMBER OF SEATS</td>
+                <td className="text-end">{`${currentCar.number_of_seats}`}</td>
               </tr>
               <tr>
-                <td>DURATION</td>
-                <td>{`${currentCar.duration} Months`}</td>
+                <td className="fw-bold">DURATION</td>
+                <td className="text-end">{`${currentCar.duration} Months`}</td>
               </tr>
             </tbody>
           </Table>
-          <h4>Description</h4>
-          <p>{currentCar.description}</p>
-          <Button variant="success">
-            <i className="far fa-bookmark" />
+          <h4 className="fsize-15 fw-bold text-end">Description</h4>
+          <p className="fsize-12 text-end">{currentCar.description}</p>
+          <Button variant="success" className="rounded-pill p-3 bg-green fsize-20">
+            <i className="far fa-bookmark me-3" />
             RESERVE
-            <i className="fa fa-arrow-circle-right" />
+            <i className="fa fa-arrow-circle-right ms-3" />
           </Button>
         </div>
       </div>
