9b55cd9f334d594c66f39c003c8a2274e0d427c8
PolinaStamenova <polina.stamenova@gmail.com>
2021-12-10 21:21:17 +0200

    Create addCar action and reducer
    The reducer is used to change the
    previuos state ot the oject car with the new one that comes
    from the action and sent to the store.

---
+++ src/redux/cars/cars.js
@@ -0,0 +1,17 @@
+const ADD_CAR = "ADD_CAR";
+
+export const addCar = (payload) => ({
+  type: ADD_CAR,
+  payload,
+});
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_CAR:
+      return [...state, ...action.payload];
+    default:
+      return state;
+  }
+};
+
+export default reducer;
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,12 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import auth from './slices/auth';
-import cars from './slices/cars';
+import { combineReducers } from "@reduxjs/toolkit";
+import auth from "./slices/auth";
+import cars from "./slices/cars";
+import addcars from "./cars/cars";
 
 const reducer = combineReducers({
-  auth, cars,
+  auth,
+  cars,
+  addcars,
 });
 
 export default reducer;
