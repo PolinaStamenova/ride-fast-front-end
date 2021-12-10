b76cf2799166b1b6ada7d993c0c0cd081cb48ae6
PolinaStamenova <polina.stamenova@gmail.com>
2021-12-10 19:43:20 +0200

    Add route for AddItem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Login from './components/HomePage/Login';
 import SignUp from './components/HomePage/SignUp';
 import HomePage from './components/HomePage/HomePage';
+import AddItem from './components/AddItemPage/AddItem';
 import 'react-toastify/dist/ReactToastify.css';
 
-// After adding the proper routes -> please, remove line 1 and this comment.
 const App = () => (
   <div className="app">
     <ToastContainer />
@@ -16,10 +15,10 @@ const App = () => (
         <Route path="/signup" exact element={<SignUp />} />
         <Route path="/login" exact element={<Login />} />
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/cars" exact element={<HomePage />} />
+        {/* <Route path='/' element={<Home />} /> */}
         {/* <Route exact path='/main' element={<Main />} /> */}
         {/* <Route exact path='/myreservations' element={<MyReservations />} /> */}
-        {/* <Route exact path='/addItem' element={<AddItem />} /> */}
+        <Route exact path="/addItem" element={<AddItem />} />
         {/* <Route exact path='/deleteItem' element={<DeleteItem />} /> */}
         {/* <Route exact path='/reserve' element={<ReserveItem />} /> */}
       </Routes>
