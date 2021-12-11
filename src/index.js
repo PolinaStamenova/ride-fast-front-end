6a83a265ff111f1110effa35c98fa2999e0889ea
Hiro Mataba <aganzemataba@gmail.com>
2021-12-11 17:23:34 +0200

    Update Route

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
-import { fetchCarsDataAsync } from './redux/slices/cars';
 
-store.dispatch(fetchCarsDataAsync());
+// store.dispatch(fetchCarsDataAsync());
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
+    <ReduxProvider store={store()}>
       <App />
     </ReduxProvider>
   </React.StrictMode>,
