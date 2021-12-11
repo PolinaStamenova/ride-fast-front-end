f558e26a90ae6f194cfcfb957b292f88f83d0e80
Hiro Mataba <aganzemataba@gmail.com>
2021-12-11 17:26:43 +0200

    Add cities slice

---
+++ src/redux/slices/cities.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/prefer-default-export */
+import { createSlice } from '@reduxjs/toolkit';
+
+const cities = createSlice({
+  name: 'cities',
+  initialState: {
+    list: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    citiesRequested: (state) => {
+      state.loading = true;
+    },
+    setCities: (state, action) => {
+      state.list = action.payload.data;
+      state.loading = false;
+    },
+    setCitiesFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
+
+export const { citiesRequested, setCities, setCitiesFailure } = cities.actions;
+export default cities.reducer;
