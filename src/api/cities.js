7bbbd5a6058f65269eac1204e9fbd419af9f6e96
Hiro Mataba <aganzemataba@gmail.com>
2021-12-11 17:24:57 +0200

    Create cities api

---
+++ src/api/cities.js
@@ -0,0 +1,7 @@
+/* eslint-disable max-len */
+/* eslint-disable no-return-await */
+import http from '../services/httpService';
+import { BASE_URL } from '../utils/constants';
+
+export const getCitiesAsync = async () => await http.get(`${BASE_URL}/cities`);
+export const getCityAsync = async (cityId) => await http.get(`${BASE_URL}/cities/${cityId}`);
